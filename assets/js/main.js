385ec1e5ebdd7ee7fe8414494ad8372385d726ef
freetimezp <kraftukrnet@ukr.net>
2025-02-07 22:42:39 +0200

    04. js-portfolio-166-construction. Home section.

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,14 @@ const linkAction = () => {
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
+
+/*-- change background header --*/
+const bgHeader = () => {
+    const header = document.getElementById("header");
+
+    this.scrollY >= 50
+        ? header.classList.add('bg-header')
+        : header.classList.remove('bg-header');
+}
+
+window.addEventListener('scroll', bgHeader);
